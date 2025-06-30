453065ae41321da5839b71585d787638a4416b86
melisklc0 <klmelis@hotmail.com>
2025-06-30 13:50:01 +0300

    Added conversational chat and chat history

--- main.py
+++ main.py
@@ -7,45 +7,53 @@ from modules.retriever import get_relevant_documents
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.schema.output_parser import StrOutputParser
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 
 def main():
     data_path = r"D:\Üniversite\Internship-Studies\Langchain-Studies\Langchain-Assistant\docs\bilgisayar-aglari"
     chroma_path = r"D:\Üniversite\Internship-Studies\Langchain-Studies\Langchain-Assistant\vectorstore\chroma_db_with_metadata"
     
-
+    load_dotenv()
     llm = get_llm()
+    db = get_chroma_db(chroma_path, data_path)
 
-    documents = load_pdfs(data_path)
-    chunks = split_text(documents)
-    db = get_chroma_db(chroma_path, chunks)
 
-    query = "UDP protokolünün genel yapısı nasıldır'?"
-    context_text = get_relevant_documents(query, db, 5, 0.7)
+    chat_history = [
+        SystemMessage(
+            content="Sen bir üniversitede asistansın." 
+            "Öğrencilerden gelen sorulara az ve öz cevap veriyorsun."
+            "Yalnızca sağlanan context'e göre cevap ver."
+        )
+    ]
 
     prompt = ChatPromptTemplate.from_template(
-        "Sen bir üniversitede asistansın. Öğrencilerden gelen sorulara cevap vererek konu anlatıyorsun."
-        "Bu konuları detaylı bir şekilde anlatman gerekiyor."
-        "\n\n{context}\n\nSoru: {question}\n\nCevap:" 
+        "Sen bir üniversitede asistansın." 
+        "Öğrencilerden gelen sorulara az ve öz cevap veriyorsun."
+        "Yalnızca sağlanan context'e göre cevap ver."
+        "\n\n{context}\n\nSoru: {question}\n\nCevap:"
     )
 
     chain = prompt | llm | StrOutputParser()
-    response = chain.invoke({"context": context_text, "question": query})
-    print("\nModelin cevabı:")
-    print(response)
+
+    while True:
+        query = input("Soru: ")
+        if query.lower() == "çıkış":
+            break
+
+        context_text = get_relevant_documents(query, db)
+        response = chain.invoke({"context": context_text, "question": query})
+
+        chat_history.append(HumanMessage(content=query))
+        chat_history.append(AIMessage(content=response))
+
+        print(f"Cevap: {response} \n")
+
+    print("\n---- Sohbet Geçmişi ----")
+    for message in chat_history:
+        print(f"{message.type.capitalize()}: {message.content}")
 
 
     
-
-
-    
-    
-
-
-      
-
-    
-
 if __name__ == "__main__":
-    load_dotenv()
     main()
